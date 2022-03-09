bcfcb0c75ba9ec80c16ac0c5ada9ab50b4725cea
Brian Pham <brianp42470@gmail.com>
2022-03-09 14:26:52 -0800

    Attemped Change

--- tracker.py
+++ tracker.py
@@ -1,7 +1,6 @@
-class func_counter(object):
-    def func_counter(self, func):
-        def count(x):
-            count.counter += 1
-            return func(x)
-        count.counter = 0
-        return count
+def func_counter(func):
+    def wrapper(arg):
+        wrapper.counter += 1
+        return func(arg)
+    wrapper.counter = 0
+    return wrapper
