a1820c5383de7855d03cdcd7a8ea67c9f4da2c3b
Brian Pham <brianp42470@gmail.com>
2022-03-09 15:33:05 -0800

    Testing

--- tracker.py
+++ tracker.py
@@ -1,4 +1,18 @@
 def func_counter(func):
-      func_counter.counter +=1
-      return func_counter
-func_counter.counter = 0
+    def wrapper(*args):
+        wrapper.counter += 1
+        return func(*args)
+    wrapper.counter = 0
+    return wrapper
+
+
+
+
+"""class func_counter:
+    def __init__(self, counter=0):
+        self.counter = counter
+    def __call__(self, func):
+        def wrapper(*args):
+            self.counter += 1
+            return func(*args)
+        return wrapper"""
