c2cf4bfbb48a229e0c93fa21be7ae38dfde07ed4
Brian Pham <brianp42470@gmail.com>
2022-03-09 13:43:11 -0800

    Fix sort

--- sort.py
+++ sort.py
@@ -1,12 +1,14 @@
 def sort_list(AList):
     n = len(AList)
     i = 0
+    k = n-i-1
     while i < n:
-        j = n - i - 1
-        while j < n:
-            temp = AList[i]
-            AList[i] = AList[j]
-            AList[j] = temp
-        j = j + 1
-    i += 1 
+        j = 0
+        while j < k:
+            if AList[i] < AList[j]:
+                temp = AList[j]
+                AList[j] = AList[i]
+                AList[i] = temp
+            j += 1
+        i += 1
     return AList
