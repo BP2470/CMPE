3d3b61cc053cac37cce15986d456c7f09d861293
Brian Pham <brianp42470@gmail.com>
2022-03-09 16:58:31 -0800

    Format fix

--- document_analyzer.py
+++ document_analyzer.py
@@ -16,28 +16,12 @@ def word_counting(str):
             count[word] = 1
     return count
 
-""""
-        if word in count:
-            count[word] += 1
-        else:
-            count[word] = 1
-    return count
-
-def letters_only(list):
-    for word in list:
-        temp = ""
-        for i in word:
-            if i.isalpha():
-                temp = "".join([temp, i])
-                list = temp
-    return list
-"""
 word_counting(d)
 
 f.close()
 count = dict(sorted(count.items(), key=lambda item: (-item[1],item[0])))
 for key, value in count.items():
-    print(key,': ',value)
+    print(str(key) + ': '+ str(value))
 
 
 
