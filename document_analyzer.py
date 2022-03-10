9a4d5fe51481c49ca9b488765fa73a1436ea821f
Brian Pham <brianp42470@gmail.com>
2022-03-09 17:51:45 -0800

    A Changes

--- document_analyzer.py
+++ document_analyzer.py
@@ -10,7 +10,12 @@ def word_counting(str):
             if i.isalpha():
                 temp = "".join([temp, i])
                 word = temp
-        if word in count:
+        if word not in count and (word == 'a'):
+            word = 'A'
+            count[word] += 1
+        elif word in count and (word != 'a' or 'A'):
+            count[word] += 1
+        elif word in count and (word == 'A'):
             count[word] += 1
         else:
             count[word] = 1
@@ -19,7 +24,7 @@ def word_counting(str):
 word_counting(d)
 f.close()
 
-count = dict(sorted(count.items(), key=lambda item: (-item[1],item[0]))[:5])
+count = dict(sorted(count.items(), key=lambda item: (-item[1],item[0])))
 
 for key, value in count.items():
     print(str(key) + ': '+ str(value))
