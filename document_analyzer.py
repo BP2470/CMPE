43b6ec33c1048405135fe42f97b7c437b67b3d66
Brian Pham <brianp42470@gmail.com>
2022-03-09 16:52:49 -0800

    Document.txt checking

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,18 +1,43 @@
-f = open("document.txt","r")
+f = open("document.txt","r",encoding='utf-8')
 d = f.read()
 count = dict()
+temp = ""
 def word_counting(str):
     words = str.split()
     for word in words:
+        temp = ""
+        for i in word:
+            if i.isalpha():
+                temp = "".join([temp, i])
+                word = temp
         if word in count:
             count[word] += 1
         else:
             count[word] = 1
     return count
 
+""""
+        if word in count:
+            count[word] += 1
+        else:
+            count[word] = 1
+    return count
+
+def letters_only(list):
+    for word in list:
+        temp = ""
+        for i in word:
+            if i.isalpha():
+                temp = "".join([temp, i])
+                list = temp
+    return list
+"""
 word_counting(d)
+
+f.close()
+count = dict(sorted(count.items(), key=lambda item: (-item[1],item[0])))
 for key, value in count.items():
-    print(key, ': ', value)
+    print(key,': ',value)
 
 
 
