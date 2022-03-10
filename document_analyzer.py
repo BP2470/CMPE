b7bfa9416b11e84012dfa1a6ffaac1dedcea1f58
Brian Pham <brianp42470@gmail.com>
2022-03-09 18:19:48 -0800

    Fix .com

--- document_analyzer.py
+++ document_analyzer.py
@@ -10,6 +10,13 @@ def word_counting(str):
             if i.isalpha():
                 temp = "".join([temp, i])
                 word = temp
+            if i == '.':
+                if word.find('com'):
+                    temp = "".join([temp, i])
+                    word = temp
+                else:
+                    temp = "".join([temp, i])
+                    word = temp
         if word not in count and (word == 'a'):
             word = 'A'
             count[word] += 1
@@ -24,7 +31,7 @@ def word_counting(str):
 word_counting(d)
 f.close()
 
-count = dict(sorted(count.items(), key=lambda item: (-item[1],item[0]))[:5])
+count = dict(sorted(count.items(), key=lambda item: (-item[1],item[0])))
 
 for key, value in count.items():
     print(str(key) + ': '+ str(value))
